5c7817a297bb257226a0ca3822460bca6b6ab0cc
Danilo Colasso <danilo.colasso@gmail.com>
2024-11-29 00:14:30 -0300

    Fix login page light background

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -9,7 +9,7 @@ export const Login: React.FC = () => {
   const { handleSubmit, credentials, setCredentials } = useLogin()
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-900">
+    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 dark:bg-gray-900">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <img
           alt="Escape 60'"
