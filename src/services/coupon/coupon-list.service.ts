ee3f8445b59d196a18a1024d6c41d4e907031363
Danilo Colasso <danilo.colasso@gmail.com>
2025-03-20 22:03:57 -0300

    fix: update import path for CouponList type to coupon-list

--- src/services/coupon/coupon-list.service.ts
+++ src/services/coupon/coupon-list.service.ts
@@ -1,7 +1,7 @@
 import api from '@/services/api'
 import { PaginatedPayload } from '@/types/paginated-payload'
 import { PaginatedResponse } from '@/types/paginated-response'
-import { CouponList } from '@/types/coupon'
+import { CouponList } from '@/types/coupon-list'
 export interface CouponListPayload extends PaginatedPayload {}
 
 export interface CouponListResponse extends PaginatedResponse<CouponList> {}
